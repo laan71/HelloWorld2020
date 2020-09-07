eb4313f887e980e5535d45edeb69fb3a91ffe821
laan71 <lail5198@edu.easj.dk>
2020-09-07 11:21:54 +0200

    Opgave lavet i undervisningstimen.

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    // psvm
+    public static void main(String[] args) {
+    // sout
+        System.out.println("Hello World!");
+        System.out.println("Hello Mor!");
+        System.out.println("5");
+        System.out.println("Hej Far!");
+    }
+}
